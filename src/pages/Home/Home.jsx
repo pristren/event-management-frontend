894e819ff1ee09b79ea0eb18eef3c3af52045b91
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2023-11-15 11:09:22 +0600

    added tailwind css

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-white bg-slate-500 font-semibold">Hellos</div>;
+};
+
+export default Home;
---
+++ src/pages/Home/HomeNavbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeNavbar = () => {
+  return <div></div>;
+};
+
+export default HomeNavbar;
